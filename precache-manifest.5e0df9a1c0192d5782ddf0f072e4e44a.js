5149e1965366b0c56411a0a598f5514d69e887d9
muniratashrifwala <munira.i.tashrifwala@gmail.com>
2020-08-31 12:05:03 +0530

    Updates

---
+++ precache-manifest.5e0df9a1c0192d5782ddf0f072e4e44a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f2763b944f1cc564a8e483ed78d031c",
+    "url": "/Notes/index.html"
+  },
+  {
+    "revision": "f93d8f72f0bf63309c87",
+    "url": "/Notes/static/css/main.34f20c99.chunk.css"
+  },
+  {
+    "revision": "c6cf6b1b94b3a19f74bf",
+    "url": "/Notes/static/js/2.3ccca73a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Notes/static/js/2.3ccca73a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f93d8f72f0bf63309c87",
+    "url": "/Notes/static/js/main.697fcbc9.chunk.js"
+  },
+  {
+    "revision": "66efa363fa8d56764ed4",
+    "url": "/Notes/static/js/runtime-main.d9f0e0c5.js"
+  }
+]);
